a825e4e9d17020408fdb5627399b8459d20708e8
Xantech007 <alexkosi2002@gmail.com>
2025-05-25 01:58:08 +0200

    update connect.php

--- account/inc/connect.php
+++ account/inc/connect.php
@@ -1,17 +1,15 @@
 <?php
-$servername = "localhost";
-// Enter your MySQL username below(default=root)
-$username = "root";
-// Enter your MySQL password below
-$password = "";
-$dbname = "primeass_db";
+$servername = "sql201.infinityfree.com"; // Correct host
+$username = "if0_39045086";              // Correct user
+$password = "Xgyuc8McZpz8Rr";            // Correct password
+$dbname = "if0_39045086_hyip_db";        // Correct database name
 
 // Create connection
 $conne = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conne->connect_error) {
-    header("location:connection_error_page.php?error=$conn->connect_error");
+    header("location:connection_error.php?error=" . $conne->connect_error);
     die($conne->connect_error);
 }
 ?>
