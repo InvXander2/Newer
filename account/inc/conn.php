47d1a6d29a13cff6488f92a405168caa4cbdefe5
Xantech007 <alexkosi2002@gmail.com>
2025-05-25 02:03:11 +0200

    update conn.php

--- account/inc/conn.php
+++ account/inc/conn.php
@@ -1,47 +1,33 @@
 <?php
 
-Class Database{
- 
-	private $server = "mysql:host=localhost;dbname=primeass_db";
-	private $username = "root";
-	private $password = "";
-	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
-	protected $conn;
- 	
-	public function open(){
- 		try{
- 			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
- 			return $this->conn;
- 		}
- 		catch (PDOException $e){
- 			echo "There is some problem in connection: " . $e->getMessage();
- 		}
- 
+class Database {
+
+        private $server = "mysql:host=sql201.infinityfree.com;dbname=if0_39045086>
+        private $username = "if0_39045086";
+        private $password = "Xgyuc8McZpz8Rr";
+        private $options  = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        );
+        protected $conn;
+
+        public function open(){
+                try {
+                        $this->conn = new PDO($this->server, $this->username, $th>
+                        return $this->conn;
+                } catch (PDOException $e) {
+                        // Log error to a file
+                        error_log("Database connection error: " . $e->getMessage(>
+                        die("Database connection failed.");
+                }
     }
- 
-	public function close(){
-   		$this->conn = null;
- 	}
- 
+
+        public function close(){
+                $this->conn = null;
+        }
 }
 
 $pdo = new Database();
 
-
-$servername = "localhost";
-// Enter your MySQL username below(default=root)
-$username = "root";
-// Enter your MySQL password below
-$password = "";
-$dbname = "primeass_db";
-
-// Create connection
-$conne = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conne->connect_error) {
-    header("location:connection_error.php?error=$conn->connect_error");
-    die($conne->connect_error);
-}
- 
 ?>
+
