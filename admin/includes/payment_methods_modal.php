45c4431ab56766ff2a97f0086c6c59521d594867
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-05-26 09:05:25 +0200

    Update payment_methods_modal.php

--- admin/includes/payment_methods_modal.php
+++ admin/includes/payment_methods_modal.php
@@ -1,4 +1,4 @@
-<!-- Add -->
+<!-- Add Payment Method Modal -->
 <div class="modal fade" id="addnew">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -16,51 +16,6 @@
         </div>
         <div class="modal-footer">
           <button type="submit" class="btn btn-primary" name="add">Save</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-
-<!-- Edit -->
-<div class="modal fade" id="edit">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <form method="POST" action="payment_methods_edit.php">
-        <div class="modal-header">
-          <h4 class="modal-title">Edit Payment Method</h4>
-        </div>
-        <div class="modal-body">
-          <input type="hidden" class="payid" name="id">
-          <div class="form-group">
-            <label>Name</label>
-            <input type="text" class="form-control" id="edit_name" name="name" required>
-            <label>Wallet Address</label>
-            <input type="text" class="form-control" id="edit_wallet" name="wallet" required>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="submit" class="btn btn-success" name="edit">Update</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-
-<!-- Delete -->
-<div class="modal fade" id="delete">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <form method="POST" action="payment_methods_delete.php">
-        <div class="modal-header">
-          <h4 class="modal-title">Delete Payment Method</h4>
-        </div>
-        <div class="modal-body">
-          <input type="hidden" class="payid" name="id">
-          <p>Are you sure you want to delete <strong class="del_name"></strong>?</p>
-        </div>
-        <div class="modal-footer">
-          <button type="submit" class="btn btn-danger" name="delete">Delete</button>
         </div>
       </form>
     </div>
