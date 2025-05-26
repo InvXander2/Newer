056738a7cc0e02efdebea46dae0db32fc953c1eb
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-05-26 20:04:02 +0200

    Update deposits-complete-request.php

--- account/deposits-complete-request.php
+++ account/deposits-complete-request.php
@@ -1,7 +1,5 @@
-
 <?php
     include('../inc/config.php');
-
     include '../admin/session.php';
 
     $page_name = 'Deposits';
@@ -11,8 +9,6 @@
 
     include('inc/head.php');
 
-    
-
     $id = $_SESSION['user'];
 
     if(!isset($_SESSION['user'])){
@@ -21,8 +17,8 @@
 
     if (isset($_POST['payNow'])) {
         $deposit_amount = $_POST['deposit_amount'];
-        $payment_mode = $_POST['payment_mode'];
-    }else{
+        $payment_method = $_POST['payment_methods'];
+    } else {
         header("location: deposits.php");
     }
 
@@ -30,28 +26,25 @@
 
     $deposit_madeQuery = $conn->query("SELECT * FROM request WHERE user_id=$id && type=1 order by 1 desc ");
     if ($deposit_madeQuery->rowCount()) {
-       $deposit_made = $deposit_madeQuery->fetchAll(PDO::FETCH_OBJ);
+        $deposit_made = $deposit_madeQuery->fetchAll(PDO::FETCH_OBJ);
     }
 
-    $payment_methodQuery = $conn->query("SELECT * FROM payment_methods");
-    if ($payment_methodQuery->rowCount()) {
-       $payment_method = $payment_methodQuery->fetchAll(PDO::FETCH_OBJ);
+    $payment_methodsQuery = $conn->query("SELECT * FROM payment_methods");
+    if ($payment_methodsQuery->rowCount()) {
+        $payment_methods = $payment_methodsQuery->fetchAll(PDO::FETCH_OBJ);
     }
 
-    $payment_completeQuery = $conn->query("SELECT * FROM payment_methods order by 1 asc Limit 1");
-    if ($payment_completeQuery->rowCount()) {
-       $payment_complete = $payment_completeQuery->fetchAll(PDO::FETCH_OBJ);
-    }
+    $payment_completeQuery = $conn->prepare("SELECT * FROM payment_methods WHERE name = :payment_method LIMIT 1");
+    $payment_completeQuery->execute(['payment_method' => $payment_method]);
+    $payment_complete = $payment_completeQuery->fetchAll(PDO::FETCH_OBJ);
 
     $depositHistory = "SELECT * FROM transaction WHERE user_id = $id && type = 1";
-
 ?>
 
-  <body>
+<body>
     <!-- Left Sidenav -->
     <?php include('inc/sidebar.php'); ?>
     <!-- end left-sidenav-->
-    
 
     <div class="page-wrapper">
         <!-- Top Bar Start -->
@@ -84,28 +77,27 @@
 
                 <?php
                     if(isset($_SESSION['error'])){
-                      echo "
+                        echo "
                         <div class='alert alert-danger border-0' role='alert'>
                             <i class='la la-skull-crossbones alert-icon text-danger align-self-center font-30 mr-3'></i>
                             <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
                                 <span aria-hidden='true'><i class='mdi mdi-close align-middle font-16'></i></span>
                             </button>
                             <strong>Oh snap!</strong> ".$_SESSION['error']."
-                        </div>
-                      ";
-                      unset($_SESSION['error']);
+                        </div>";
+                        unset($_SESSION['error']);
                     }
+
                     if(isset($_SESSION['success'])){
-                      echo "
+                        echo "
                         <div class='alert alert-success border-0' role='alert'>
                             <i class='mdi mdi-check-all alert-icon'></i>
                             <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
                                 <span aria-hidden='true'><i class='mdi mdi-close align-middle font-16'></i></span>
                             </button>
                             <strong>Well done!</strong> ".$_SESSION['success']."
-                        </div>
-                      ";
-                      unset($_SESSION['success']);
+                        </div>";
+                        unset($_SESSION['success']);
                     }
                 ?>
 
@@ -113,69 +105,50 @@
                     <div class="col-lg-3"></div>
                     <div class="col-lg-6">
                         <div class="card">
-
                             <div class="card-header">
                                 <h4 class="card-title">Complete Your Request</h4>
-                                
                             </div><!--end card-header--> 
                             <div class="card-body">
+                                <div class="card">
+                                    <div class="card-body"> 
+                                        <form class="form-horizontal auth-form" method="post" action="deposit-action">
+                                            <input type="number" name="deposit_amount" value="<?php echo $deposit_amount; ?>" hidden>
+                                            <input type="text" name="payment_methods" value="<?php echo $payment_method; ?>" hidden>
 
-                              
-                              <div class="card">
-                                <div class="card-body"> 
-                                    <form class="form-horizontal auth-form" method="post" action="deposit-action">
-                                        <input type="number" name="deposit_amount" value="<?php echo $deposit_amount; ?>" hidden>
-                                        <input type="text" name="payment_mode" value="<?php echo $payment_mode; ?>" hidden>
-                
-                                        <div class="form-group mb-2">
-                                            <?php
-                                              foreach ($payment_complete as $complete) : ?>
+                                            <div class="form-group mb-2">
+                                                <?php foreach ($payment_complete as $complete): ?>
+                                                    <div class="form-group">
+                                                        <img src="../admin/images/<?= $complete->photo; ?>" alt="payment QR" style="max-width: 100%;">
+                                                    </div>
 
-                                              <div class="form-group">
-                                                <img src="../admin/images/<?= $complete->photo; ?>">
-                                              </div>
+                                                    <div class="form-group">
+                                                        <label><?= $complete->wallet_address; ?></label>
+                                                    </div>
+                                                <?php endforeach; ?>
+                                            </div><!--end form-group--> 
 
-                                              <div class="form-group">
-                                                <label><?= $complete->wallet_address; ?></label>
-                                              </div>
-
-                                            <?php
-                                               endforeach; ?>
-                                                                      
-                                        </div><!--end form-group--> 
-                                                            
-                                        <div class="form-group mb-0 row">
-                                            <div class="col-12">
-                                                <button class="btn btn-primary btn-block waves-effect waves-light" type="submit" name="complete">Log Request Now <i class="fas fa-money-bill ml-1"></i></button>
-                                            </div><!--end col--> 
-                                        </div> <!--end form-group-->                           
-                                    </form><!--end form-->
+                                            <div class="form-group mb-0 row">
+                                                <div class="col-12">
+                                                    <button class="btn btn-primary btn-block waves-effect waves-light" type="submit" name="complete">
+                                                        Log Request Now <i class="fas fa-money-bill ml-1"></i>
+                                                    </button>
+                                                </div><!--end col--> 
+                                            </div> <!--end form-group-->                           
+                                        </form><!--end form-->
+                                    </div>
                                 </div>
-                              </div>
-                                                                    
                             </div><!--end card-body-->
                         </div><!--end card-->
                     </div><!--end col-->
-
                 </div><!--end row-->
-                
-
-
             </div><!-- container -->
 
-            <?php include('inc/footer.php'); ?><!--end footer-->
+            <?php include('inc/footer.php'); ?>
         </div>
         <!-- end page content -->
     </div>
     <!-- end page-wrapper -->
 
-    
     <?php include('inc/scripts.php'); ?>
-
-      
-      
-  </body>
-
-
-<!-- Mirrored from mannatthemes.com/dastone/default/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 01 Dec 2020 21:59:40 GMT -->
+</body>
 </html>
