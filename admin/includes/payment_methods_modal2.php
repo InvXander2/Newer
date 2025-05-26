3a9cec1a5ea2f2c21494a0886ab3880b76503de6
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-05-26 11:17:38 +0200

    Update payment_methods_modal2.php

--- admin/includes/payment_methods_modal2.php
+++ admin/includes/payment_methods_modal2.php
@@ -9,7 +9,7 @@
       </div>
       <div class="modal-body">
         <form class="form-horizontal" method="POST" action="payment_methods_delete.php">
-          <input type="hidden" class="methodid" name="id">
+          <input type="hidden" class="ipid" name="id">
           <div class="text-center">
             <p>DELETE Payment Method</p>
             <h2 class="bold name"></h2>
@@ -24,9 +24,10 @@
   </div>
 </div>
 
+
 <!-- Edit -->
 <div class="modal fade" id="edit">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -34,19 +35,34 @@
         <h4 class="modal-title"><b>Edit Payment Method</b></h4>
       </div>
       <div class="modal-body">
-        <form class="form-horizontal" method="POST" action="payment_methods_edit.php">
-          <input type="hidden" class="methodid" name="id">
+        <form class="form-horizontal" method="POST" action="payment_methods_edit.php" enctype="multipart/form-data">
+          <input type="hidden" class="ipid" name="id">
+          
           <div class="form-group">
-            <label for="edit_name" class="col-sm-3 control-label">Method Name</label>
-            <div class="col-sm-9">
+            <label for="edit_name" class="col-sm-2 control-label">Name</label>
+            <div class="col-sm-10">
               <input type="text" class="form-control" id="edit_name" name="name" required>
             </div>
           </div>
 
           <div class="form-group">
-            <label for="edit_wallet" class="col-sm-3 control-label">Wallet Address</label>
-            <div class="col-sm-9">
-              <input type="text" class="form-control" id="edit_wallet" name="wallet" required>
+            <label for="edit_wallet_address" class="col-sm-2 control-label">Wallet Address</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="edit_wallet_address" name="wallet_address" required>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="edit_details" class="col-sm-2 control-label">Details</label>
+            <div class="col-sm-10">
+              <textarea class="form-control" id="edit_details" name="details" rows="3" required></textarea>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="edit_photo" class="col-sm-2 control-label">Photo</label>
+            <div class="col-sm-10">
+              <input type="file" id="edit_photo" name="photo">
             </div>
           </div>
       </div>
@@ -57,4 +73,4 @@
       </div>
     </div>
   </div>
-</div>
+  </div>
