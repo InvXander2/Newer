ff19553d4b953dc6f4d6d6703ccc0cbb4ee14146
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-05-28 00:09:32 +0200

    Update profile-edit.php

--- account/profile-edit.php
+++ account/profile-edit.php
@@ -1,211 +1,148 @@
 <?php
     include('../inc/config.php');
     include('../admin/includes/format.php');
-
     include '../admin/session.php';
 
     $page_name = 'Profile';
     $page_parent = '';
     $page_title = 'Welcome to the Official Website of '.$settings->siteTitle;
-    $page_description = $settings->siteTitle.' provides quality infrastructure backed high-performance cloud computing services for cryptocurrency mining. Choose a plan to get started today! What are you waiting for? Together We Grow!...';
+    $page_description = $settings->siteTitle.' provides quality infrastructure backed high-performance cloud computing services for cryptocurrency mining.';
 
     include('inc/head.php');
-
-    $id = $_SESSION['user'];
 
     if(!isset($_SESSION['user'])){
         header('location: ../login.php');
     }
 
+    $id = $_SESSION['user'];
     $sql0 = "SELECT * FROM users WHERE id=".$id;
     $result0 = $conne->query($sql0);
     $row0 = $result0->fetch_assoc();
-
-    $referrals = $row0['uname'];
-
-    $sql1 = "SELECT * FROM transaction WHERE user_id = $id ORDER BY trans_id DESC LIMIT 1";
-    $result1 = $conne->query($sql1);
-    $row1 = $result1->fetch_assoc();
-
-    if ($row1["type"] == 1) {
-        $transaction = $row1["amount"];
-        $type = "credit";
-    }
-    else {
-        $transaction = $row1["amount"];
-        $type = "debit";
-    }
-
-    $time = strtotime($row1["trans_date"]);
-    $sanitized_time = date("Y-m-d, g:i A", $time);
-
-    
-
-    $sql2 = "SELECT *, COUNT(*) AS numrows FROM users WHERE referral_code='$referrals' ";
-    $result2 = $conne->query($sql2);
-    $row2 = $result2->fetch_assoc();
-    $no_of_referrals = $row2['numrows'];
-
 ?>
 
-     <body class="dark-topbar">
-    <!-- Left Sidenav -->
-    <?php include('inc/sidebar.php'); ?>
-    <!-- end left-sidenav-->
-    
+<body class="dark-topbar">
+<?php include('inc/sidebar.php'); ?>
 
-    <div class="page-wrapper">
-        <!-- Top Bar Start -->
-        <?php include('inc/header.php'); ?>
-        <!-- Top Bar End -->
+<div class="page-wrapper">
+    <?php include('inc/header.php'); ?>
 
-        <!-- Page Content-->
-        <div class="page-content">
-            <div class="container-fluid">
-                <!-- Page-Title -->
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="page-title-box">
-                            <div class="row">
-                                <div class="col">
-                                    <h4 class="page-title">Profile Completion</h4>
-                                </div><!--end col-->
-                                <div class="col-auto align-self-center">
-                                    <a href="#" class="btn btn-sm btn-outline-primary" id="Dash_Date">
-                                        <span class="day-name" id="Day_Name">Today:</span>&nbsp;
-                                        <span class="" id="Select_date">Jan 11</span>
-                                        <i data-feather="calendar" class="align-self-center icon-xs ml-1"></i>
-                                    </a>
-                                </div><!--end col-->  
-                            </div><!--end row-->                                                              
-                        </div><!--end page-title-box-->
-                    </div><!--end col-->
-                </div><!--end row-->
-                <!-- end page title end breadcrumb -->
+    <div class="page-content">
+        <div class="container-fluid">
 
-                <?php
-                    if(isset($_SESSION['error'])){
-                      echo "
-                        <div class='alert alert-danger border-0' role='alert'>
-                            <i class='la la-skull-crossbones alert-icon text-danger align-self-center font-30 mr-3'></i>
-                            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-                                <span aria-hidden='true'><i class='mdi mdi-close align-middle font-16'></i></span>
-                            </button>
-                            <strong>Oh snap!</strong> ".$_SESSION['error']."
-                        </div>
-                      ";
-                      unset($_SESSION['error']);
-                    }
-                    if(isset($_SESSION['success'])){
-                      echo "
-                        <div class='alert alert-success border-0' role='alert'>
-                            <i class='mdi mdi-check-all alert-icon'></i>
-                            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-                                <span aria-hidden='true'><i class='mdi mdi-close align-middle font-16'></i></span>
-                            </button>
-                            <strong>Well done!</strong> ".$_SESSION['success']."
-                        </div>
-                      ";
-                      unset($_SESSION['success']);
-                    }
-                ?>
-
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="row">
-                                <div class="col-lg-12 col-xl-12">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <div class="row align-items-center">
-                                                <div class="col">                      
-                                                    <h4 class="card-title">Complete Your Registration Now</h4>                      
-                                                </div><!--end col-->                                                       
-                                            </div>  <!--end row-->                                  
-                                        </div><!--end card-header-->
-                                        <form method="post" action="profile-edit-action" enctype="multipart/form-data">
-                                            <div class="card-body">                       
-                                                <div class="form-group row">
-                                                    <label class="col-xl-3 col-lg-3 text-right mb-lg-0 align-self-center">Gender</label>
-                                                    <div class="col-lg-9 col-xl-8">
-                                                        <select name="gender" class="form-control">
-                                                            <option selected disabled>Choose...</option>
-                                                            <option value="Male">Male</option>
-                                                            <option value="Female">Female</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group row">
-                                                    <label class="col-xl-3 col-lg-3 text-right mb-lg-0 align-self-center">Date of Birth</label>
-                                                    <div class="col-lg-9 col-xl-8">
-                                                        <input class="form-control" type="date" name="dob"  placeholder="DD-MM-YYYY">
-                                                    </div>
-                                                </div>
-                                                <div class="form-group row">
-                                                    <label class="col-xl-3 col-lg-3 text-right mb-lg-0 align-self-center">Nationality</label>
-                                                    <div class="col-lg-9 col-xl-8">
-                                                        <div class="input-group">
-                                                            <div class="input-group-prepend"><span class="input-group-text"><i class="la la-globe"></i></span></div>
-                                                            <select name="nationality" class="form-control">
-                                                                <?php include('../inc/countries.php'); ?>
-                                                            </select>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                    
-                                                <div class="form-group row">
-                                                    <label class="col-xl-3 col-lg-3 text-right mb-lg-0 align-self-center">Contact Phone</label>
-                                                    <div class="col-lg-9 col-xl-8">
-                                                        <div class="input-group">
-                                                            <div class="input-group-prepend"><span class="input-group-text"><i class="las la-phone"></i></span></div>
-                                                            <input type="text" name="phone_no" class="form-control" placeholder="Enter Phone Number" >
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group row">
-                                                    <label class="col-xl-3 col-lg-3 text-right mb-lg-0 align-self-center">Profile Picture</label>
-                                                    <div class="col-lg-9 col-xl-8">
-                                                        <div class="input-group">
-                                                            <div class="input-group-prepend"><span class="input-group-text"><i class="las la-file-upload"></i></span></div>
-                                                            
-                                                            <div class="custom-file">
-                                                                <input type="file" name="photo" class="custom-file-input" id="customFile">
-                                                                <label class="custom-file-label" for="customFile">Choose file</label>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group row">
-                                                    <div class="col-lg-9 col-xl-8 offset-lg-3">
-                                                        <button type="submit" name="update" class="btn btn-primary">Submit</button>
-                                                    </div>
-                                                </div>                                                    
-                                            </div>   
-                                        </form>
-                                                                                 
-                                    </div>
-                                </div> <!--end col--> 
-                                
-                                                                      
-                            </div><!--end row-->
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="page-title-box">
+                        <div class="row">
+                            <div class="col">
+                                <h4 class="page-title">Profile Completion</h4>
+                            </div>
+                            <div class="col-auto align-self-center">
+                                <a href="#" class="btn btn-sm btn-outline-primary">
+                                    <span class="day-name">Today:</span>
+                                    <span id="Select_date"><?php echo date("M d"); ?></span>
+                                    <i data-feather="calendar" class="align-self-center icon-xs ml-1"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
+                </div>
+            </div>
 
+            <?php
+                if(isset($_SESSION['error'])){
+                    echo "<div class='alert alert-danger'>{$_SESSION['error']}</div>";
+                    unset($_SESSION['error']);
+                }
+                if(isset($_SESSION['success'])){
+                    echo "<div class='alert alert-success'>{$_SESSION['success']}</div>";
+                    unset($_SESSION['success']);
+                }
+            ?>
 
-            </div><!-- container -->
+            <div class="row">
+                <div class="col-lg-12 col-xl-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Update Profile Information</h4>
+                        </div>
+                        <form method="post" action="profile-edit-action.php" enctype="multipart/form-data">
+                            <div class="card-body">
 
-            <?php include('inc/footer.php'); ?><!--end footer-->
+                                <div class="form-group row">
+                                    <label class="col-xl-3 col-lg-3 text-right align-self-center">Full Name</label>
+                                    <div class="col-lg-9 col-xl-8">
+                                        <input class="form-control" type="text" name="full_name" value="<?= $row0['full_name'] ?>" required>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-xl-3 col-lg-3 text-right align-self-center">Username</label>
+                                    <div class="col-lg-9 col-xl-8">
+                                        <input class="form-control" type="text" name="uname" value="<?= $row0['uname'] ?>" required>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-xl-3 col-lg-3 text-right align-self-center">Password</label>
+                                    <div class="col-lg-9 col-xl-8">
+                                        <input class="form-control" type="password" name="password" placeholder="Enter new password (leave blank to keep current)">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-xl-3 col-lg-3 text-right align-self-center">Date of Birth</label>
+                                    <div class="col-lg-9 col-xl-8">
+                                        <input class="form-control" type="date" name="dob" value="<?= $row0['dob'] ?>">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-xl-3 col-lg-3 text-right align-self-center">Nationality</label>
+                                    <div class="col-lg-9 col-xl-8">
+                                        <select name="nationality" class="form-control">
+                                            <?php include('../inc/countries.php'); ?>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-xl-3 col-lg-3 text-right align-self-center">Address</label>
+                                    <div class="col-lg-9 col-xl-8">
+                                        <input class="form-control" type="text" name="address" value="<?= $row0['address'] ?>">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-xl-3 col-lg-3 text-right align-self-center">Phone Number</label>
+                                    <div class="col-lg-9 col-xl-8">
+                                        <input type="text" name="phone_no" class="form-control" value="<?= $row0['phone_no'] ?>" required>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-xl-3 col-lg-3 text-right align-self-center">Profile Picture</label>
+                                    <div class="col-lg-9 col-xl-8">
+                                        <input type="file" name="photo" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <div class="col-lg-9 col-xl-8 offset-lg-3">
+                                        <button type="submit" name="update" class="btn btn-primary">Update Profile</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
         </div>
-        <!-- end page content -->
+        <?php include('inc/footer.php'); ?>
     </div>
-    <!-- end page-wrapper -->
+</div>
 
-    
-    <?php include('inc/scripts.php'); ?>
-
-      
-      
-  </body>
-
-
-<!-- Mirrored from mannatthemes.com/dastone/default/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 01 Dec 2020 21:59:40 GMT -->
+<?php include('inc/scripts.php'); ?>
+</body>
 </html>
