63b42f2a10ef1e918d3d9f0f315b8a19f8f83ad8
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-05-31 00:13:20 +0200

    Update conn.php

--- inc/conn.php
+++ inc/conn.php
@@ -2,7 +2,7 @@
 
 class Database {
  
-	private $server = "mysql:host=sql201.infinityfree.com;dbname=if0_39045086_hyip_db";
+	private $server = "mysql:host=sql201.infinityfree.com;dbname=if0_39045086_Nexusinsights";
 	private $username = "if0_39045086";
 	private $password = "Xgyuc8McZpz8Rr";
 	private $options  = array(
