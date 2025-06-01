fd5d40af4540a4e6e46dde3ecec3e10faaa68811
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-06-01 11:18:26 +0200

    Delete account/inc/checklogin.php

--- account/inc/checklogin.php
+++
@@ -1,9 +0,0 @@
-<?php
-function check_login() {
-    if (!isset($_SESSION['user']) || empty($_SESSION['user'])) {
-        $_SESSION['error'] = "Please log in to access the dashboard.";
-        header('location: ../login.php');
-        exit();
-    }
-}
-?>
