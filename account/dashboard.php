90ab36dc97615a825a302d48b9c56f0a6fdd5f3d
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-06-01 06:31:51 +0200

    Update dashboard.php

--- account/dashboard.php
+++ account/dashboard.php
@@ -21,31 +21,6 @@ if (!function_exists('number_format_short')) {
     }
 }
 
-if (!function_exists('substrwords')) {
-    function substrwords($text, $maxchar, $end='...') {
-        if (strlen($text) > $maxchar || $text == '') {
-            $words = preg_split('/\s/', $text);      
-            $output = '';
-            $i      = 0;
-            while (1) {
-                $length = strlen($output)+strlen($words[$i]);
-                if ($length > $maxchar) {
-                    break;
-                } 
-                else {
-                    $output .= " " . $words[$i];
-                    ++$i;
-                }
-            }
-            $output .= $end;
-        } 
-        else {
-            $output = $text;
-        }
-        return $output;
-    }
-}
-
 // Initialize database connection
 try {
     $conn = $pdo->open();
