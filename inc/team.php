bbf23ef17848c0a2169a4a690572de640d8ef513
Xantech007 <alexkosi2002@gmail.com>
2025-06-03 01:40:50 +0200

    update

--- inc/team.php
+++ inc/team.php
@@ -1,102 +1,85 @@
 <section class="pt-120 pb-120 bg_img" data-background="assets/images/bg/bg-5.jpg">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-6 text-center">
-            <div class="section-header">
-              <h2 class="section-title"><span class="font-weight-normal">Our Expert</span> <b class="base--color">Team Members</b></h2>
-              
-            </div>
-          </div>
-        </div><!-- row end -->
-        <div class="row justify-content-center mb-none-30">
-          <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
-            <div class="team-card">
-              <div class="team-card__thumb">
-                <img src="assets/images/investor/1.jpg" alt="image">
-              </div>
-              <div class="team-card__content">
-                <h4 class="name mb-1">Roberto Bani</h4>
-                <span class="designation">Founder</span>
-              </div>
-            </div><!-- team-card end -->
-          </div>
-          <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
-            <div class="team-card">
-              <div class="team-card__thumb">
-                <img src="assets/images/investor/2.jpg" alt="image">
-              </div>
-              <div class="team-card__content">
-                <h4 class="name mb-1">Danial K.</h4>
-                <span class="designation">Co-Founder</span>
-              </div>
-            </div><!-- team-card end -->
-          </div>
-          <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
-            <div class="team-card">
-              <div class="team-card__thumb">
-                <img src="assets/images/investor/3.jpg" alt="image">
-              </div>
-              <div class="team-card__content">
-                <h4 class="name mb-1">Lew Son</h4>
-                <span class="designation">Marketing Head</span>
-              </div>
-            </div><!-- team-card end -->
-          </div>
-          <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
-            <div class="team-card">
-              <div class="team-card__thumb">
-                <img src="assets/images/investor/4.jpg" alt="image">
-              </div>
-              <div class="team-card__content">
-                <h4 class="name mb-1">Tend z Joe</h4>
-                <span class="designation">Designer</span>
-              </div>
-            </div><!-- team-card end -->
-          </div>
-          <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
-            <div class="team-card">
-              <div class="team-card__thumb">
-                <img src="assets/images/investor/5.jpg" alt="image">
-              </div>
-              <div class="team-card__content">
-                <h4 class="name mb-1">Sam Joe</h4>
-                <span class="designation">Developer</span>
-              </div>
-            </div><!-- team-card end -->
-          </div>
-          <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
-            <div class="team-card">
-              <div class="team-card__thumb">
-                <img src="assets/images/investor/6.jpg" alt="image">
-              </div>
-              <div class="team-card__content">
-                <h4 class="name mb-1">Alex Joe</h4>
-                <span class="designation">UX Expert</span>
-              </div>
-            </div><!-- team-card end -->
-          </div>
-          <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
-            <div class="team-card">
-              <div class="team-card__thumb">
-                <img src="assets/images/investor/7.jpg" alt="image">
-              </div>
-              <div class="team-card__content">
-                <h4 class="name mb-1">Juna Sun</h4>
-                <span class="designation">SEO Expert</span>
-              </div>
-            </div><!-- team-card end -->
-          </div>
-          <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
-            <div class="team-card">
-              <div class="team-card__thumb">
-                <img src="assets/images/investor/8.jpg" alt="image">
-              </div>
-              <div class="team-card__content">
-                <h4 class="name mb-1">Profed Laun</h4>
-                <span class="designation">Manager</span>
-              </div>
-            </div><!-- team-card end -->
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-6 text-center">
+        <div class="section-header">
+          <h2 class="section-title"><span class="font-weight-normal">Our Expert</span> <b class="base--color">Team Members</b></h2>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-center mb-none-30">
+      <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
+        <div class="team-card">
+          <div class="team-card__thumb team-img-1"></div>
+          <div class="team-card__content">
+            <h4 class="name mb-1">Roberto Bani</h4>
+            <span class="designation">Founder</span>
           </div>
         </div>
       </div>
-    </section>
+      <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
+        <div class="team-card">
+          <div class="team-card__thumb team-img-2"></div>
+          <div class="team-card__content">
+            <h4 class="name mb-1">Danial K.</h4>
+            <span class="designation">Co-Founder</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
+        <div class="team-card">
+          <div class="team-card__thumb team-img-3"></div>
+          <div class="team-card__content">
+            <h4 class="name mb-1">Lew Son</h4>
+            <span class="designation">Marketing Head</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
+        <div class="team-card">
+          <div class="team-card__thumb team-img-4"></div>
+          <div class="team-card__content">
+            <h4 class="name mb-1">Tend z Joe</h4>
+            <span class="designation">Designer</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
+        <div class="team-card">
+          <div class="team-card__thumb team-img-5"></div>
+          <div class="team-card__content">
+            <h4 class="name mb-1">Sam Joe</h4>
+            <span class="designation">Developer</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
+        <div class="team-card">
+          <div class="team-card__thumb team-img-6"></div>
+          <div class="team-card__content">
+            <h4 class="name mb-1">Alex Joe</h4>
+            <span class="designation">UX Expert</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
+        <div class="team-card">
+          <div class="team-card__thumb team-img-7"></div>
+          <div class="team-card__content">
+            <h4 class="name mb-1">Juna Sun</h4>
+            <span class="designation">SEO Expert</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
+        <div class="team-card">
+          <div class="team-card__thumb team-img-8"></div>
+          <div class="team-card__content">
+            <h4 class="name mb-1">Profed Laun</h4>
+            <span class="designation">Manager</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
