e80d5d3dc045bc6ee9c546076f58deee67a9e6c7
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-06-03 15:41:05 +0200

    Update session.php

--- inc/session.php
+++ inc/session.php
@@ -1,25 +1,24 @@
 <?php
-    include_once '../inc/conn.php';
-    $pdo = new Database();
+	include 'admin/includes/conn.php';
+	session_start();
 
-    session_start();
+	if(isset($_SESSION['admin'])){
+		header('location: admin/home.php');
+	}
 
-    if (isset($_SESSION['admin'])) {
-        header('location: admin/home.php');
-        exit();
-    }
+	if(isset($_SESSION['user'])){
+		$conn = $pdo->open();
 
-    if (isset($_SESSION['user'])) {
-        $conn = $pdo->open();
+		try{
+			$stmt = $conn->prepare("SELECT * FROM users WHERE id=:id");
+			$stmt->execute(['id'=>$_SESSION['user']]);
+			$user = $stmt->fetch();
+		}
+		catch(PDOException $e){
+			echo "There is some problem in connection: " . $e->getMessage();
+		}
 
-        try {
-            $stmt = $conn->prepare("SELECT * FROM users WHERE id = :id");
-            $stmt->execute(['id' => $_SESSION['user']]);
-            $user = $stmt->fetch();
-        } catch (PDOException $e) {
-            echo "There is some problem in connection: " . $e->getMessage();
-        }
+		$pdo->close();
+	}
 
-        $pdo->close();
-    }
 ?>
