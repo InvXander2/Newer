4dfd986724b8a573a084da7b4db23878489ea451
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-06-07 16:11:46 +0200

    Update reset.php

--- reset.php
+++ reset.php
@@ -1,91 +1,198 @@
 <?php
-    include('init.php');
-	use PHPMailer\PHPMailer\PHPMailer;
-	use PHPMailer\PHPMailer\Exception;
+include('init.php');
 
-	include 'inc/session.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-	if(isset($_POST['reset'])){
-		$email = $_POST['email'];
+include 'inc/session.php';
 
-		$conn = $pdo->open();
+if (isset($_POST['reset'])) {
+    $email = $_POST['email'];
 
-		$stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows FROM users WHERE email=:email");
-		$stmt->execute(['email'=>$email]);
-		$row = $stmt->fetch();
+    // Validate email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $_SESSION['error'] = 'Invalid email format';
+        header('location: password_forgot.php');
+        exit;
+    }
 
-		if($row['numrows'] > 0){
-			//generate code
-			$set='123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-			$code=substr(str_shuffle($set), 0, 15);
-			try{
-				$stmt = $conn->prepare("UPDATE users SET reset_code=:code WHERE id=:id");
-				$stmt->execute(['code'=>$code, 'id'=>$row['id']]);
-				
-				$message = "
-					<h2>Password Reset</h2>
-					<p>Your Account:</p>
-					<p>Email: ".$email."</p>
-					<p>Please click the link below to reset your password.</p>
-					<a href='http://".$sweet_url."/password_reset.php?code=".$code."&user=".$row['id']."'>Reset Password</a>
-				";
+    $conn = $pdo->open();
 
-				//Load phpmailer
-	    		require 'vendor/autoload.php';
+    try {
+        $stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows FROM users WHERE email = :email");
+        $stmt->execute(['email' => $email]);
+        $row = $stmt->fetch();
 
-	    		$mail = new PHPMailer(true);                             
-			    try {
-			        //Server settings
-			        $mail->isSMTP();                                     
-			        $mail->Host = $sweet_url;                      
-			        $mail->SMTPAuth = true;                               
-			        $mail->Username = 'info@'.$sweet_url;     
-			        $mail->Password = $noreply_password;                   
-			        $mail->SMTPOptions = array(
-			            'ssl' => array(
-			            'verify_peer' => false,
-			            'verify_peer_name' => false,
-			            'allow_self_signed' => true
-			            )
-			        );                         
-			        $mail->SMTPSecure = 'ssl';                           
-			        $mail->Port = 465;                                   
+        if ($row['numrows'] > 0) {
+            // Generate reset code
+            $set = '123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $code = substr(str_shuffle($set), 0, 15);
 
-			        $mail->setFrom($settings->email);
-				        
-			        //Recipients
-			        $mail->addAddress($email);              
-			        $mail->addReplyTo($settings->email);
-			       
-			        //Content
-			        $mail->isHTML(true);                                  
-			        $mail->Subject = $settings->siteTitle.' Password Reset';
-			        $mail->Body    = $message;
+            // Update user with reset code
+            $stmt = $conn->prepare("UPDATE users SET reset_code = :code WHERE id = :id");
+            $stmt->execute(['code' => $code, 'id' => $row['id']]);
 
-			        $mail->send();
+            // Email template
+            $message = <<<HTML
+<div style='font-family: Helvetica Neue, Helvetica, Roboto, Arial, sans-serif; direction: ltr; background-color: #f3f2f1; margin: 0; padding: 0;'>
+    <table class='main' border='0' width='100%' cellspacing='0' cellpadding='0' bgcolor='#F3F2F1'>
+        <tbody>
+            <tr>
+                <td class='outer-box' style='padding: 0 8px;' align='center' bgcolor='#F3F2F1'>
+                    <table style='max-width: 600px; padding: 0 0 15px 0;' border='0' width='100%' cellspacing='0' cellpadding='0'>
+                        <tbody>
+                            <tr>
+                                <td style='padding: 10px 0 13px 0;' align='left'>
+                                    <a href='https://{$sweet_url}'>
+                                        <img
+                                            style='display: block;'
+                                            src='https://{$sweet_url}/assets/images/logo-dark.png'
+                                            alt='nexus-logo'
+                                            width='300'
+                                            height='60'
+                                            border='0'
+                                        />
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class='width-600' style='max-width: 600px;' border='0' width='100%' cellspacing='0' cellpadding='0' bgcolor='#FFFFFF'>
+                        <tbody>
+                            <tr>
+                                <td class='content-box' style='padding-bottom: 24px !important;'>
+                                    <table border='0' width='100%' cellspacing='0' cellpadding='0'>
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <table border='0' width='100%' cellspacing='0' cellpadding='0'>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td style='padding: 16px 10px 0;'>
+                                                                    <p style='font-size: 13px; line-height: 20px; color: #666666; margin: 0px; text-align: left;' align='center'>
+                                                                        <span style='font-size: 12pt; font-family: arial black, sans-serif; color: #000000;'>
+                                                                            <strong>Dear {$row['full_name']} ({$row['uname']}),</strong>
+                                                                        </span>
+                                                                    </p>
+                                                                    <p style='font-size: 13px; line-height: 20px; color: #666666; margin: 0px; text-align: left;' align='center'>&nbsp;</p>
+                                                                    <p style='font-size: 13px; line-height: 20px; color: #666666; margin: 0px; text-align: left;' align='center'>
+                                                                        <span style='color: #000000;'>
+                                                                            A password reset request has been made for your account associated with <strong>{$email}</strong>.
+                                                                            <br /><br />
+                                                                            Please click the button below to reset your password:
+                                                                            <br /><br />
+                                                                            <a style='display: inline-block; padding: 10px 20px; background-color: #d60000; color: #ffffff; text-decoration: none; border-radius: 20px;' href='https://{$sweet_url}/password_reset.php?code={$code}&user={$row['id']}'>Reset Password</a>
+                                                                        </span>
+                                                                    </p>
+                                                                    <p style='font-size: 13px; line-height: 20px; color: #666666; margin: 0px; text-align: left;' align='center'>&nbsp;</p>
+                                                                    <p style='font-size: 13px; line-height: 20px; color: #666666; margin: 0px; text-align: left;' align='center'>
+                                                                        <span style='color: #000000;'>
+                                                                            Do note that Nexus Insights will not give you any other wallet address apart from the one shown on the website.
+                                                                        </span>
+                                                                        <br /><br />
+                                                                        <span style='color: #000000;'>
+                                                                            To report fraudulent activities, contact
+                                                                            <strong><a style='color: #000000;' href='mailto:{$settings->email2}'>fraud@nexusinsights.it.com</a></strong>
+                                                                        </span>
+                                                                    </p>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>&nbsp;</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table style='max-width: 550px; width: 100%;' border='0' cellspacing='0' cellpadding='0' bgcolor='#F2F2F2'>
+                        <tbody>
+                            <tr>
+                                <td style='padding: 24px 4px; width: 100%;'>
+                                    <table style='max-width: 424px;' border='0' cellspacing='0' cellpadding='0' align='center'>
+                                        <tbody>
+                                            <tr>
+                                                <td style='font-size: 12px; line-height: 16px; color: #4b4b4b; padding: 20px 0; margin: 0 auto;' align='center'>
+                                                    *This email account is not monitored. Reply to <a href='mailto:{$settings->email}'>{$settings->email}</a> if you have any query.
+                                                    <a style='text-decoration: underline; color: #085ff7;' href='https://{$sweet_url}/investment'> View Our Available Plans </a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <table style='font-size: 12px; color: #2d2d2d; line-height: 22px; margin: 0px auto; width: 100%;' border='0' width='100%' cellspacing='0' cellpadding='0' align='center'>
+                                        <tbody>
+                                            <tr>
+                                                <td lang='en' style='padding: 0px;' align='center'>© {$year} Nexus Insights Limited.</td>
+                                            </tr>
+                                            <tr>
+                                                <td style='padding: 15px 0px 25px;' align='center'>
+                                                    <span><a style='text-decoration: underline; color: #085ff7;' href='https://{$sweet_url}'>Home</a>|</span>
+                                                    <a style='text-decoration: underline; color: #085ff7;' href='https://{$sweet_url}/about'>About</a>
+                                                    <span>|</span>
+                                                    <a style='text-decoration: underline; color: #085ff7;' href='https://{$sweet_url}/investment'>Plans</a>
+                                                    <br />
+                                                    <a style='text-decoration: underline; color: #085ff7;' href='https://{$sweet_url}/news'>News</a>
+                                                    <span>|</span>
+                                                    <a style='text-decoration: underline; color: #085ff7;' href='https://{$sweet_url}/contact'>Contact</a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+HTML;
 
-			        $_SESSION['success'] = 'Password reset link sent . Please check your email.';
-			     
-			    } 
-			    catch (Exception $e) {
-			        $_SESSION['error'] = 'Message could not be sent now. Contact support for password reset';
-			    }
-			}
-			catch(PDOException $e){
-				$_SESSION['error'] = $e->getMessage();
-			}
-		}
-		else{
-			$_SESSION['error'] = 'Email not found';
-		}
+            // Send email using PHPMailer
+            require 'vendor/autoload.php';
 
-		$pdo->close();
+            $mail = new PHPMailer(true);
+            try {
+                // Server settings
+                $mail->isSMTP();
+                $mail->Host = $smtpConfig['host'];
+                $mail->SMTPAuth = true;
+                $mail->Username = $smtpConfig['username'];
+                $mail->Password = $smtpConfig['password'];
+                $mail->SMTPSecure = $smtpConfig['secure'];
+                $mail->Port = $smtpConfig['port'];
 
-	}
-	else{
-		$_SESSION['error'] = 'Input email associated with account';
-	}
+                // Recipients
+                $mail->setFrom($smtpConfig['fromEmail'], $smtpConfig['fromName']);
+                $mail->addAddress($email);
+                $mail->addReplyTo($smtpConfig['fromEmail'], $smtpConfig['fromName']);
 
-	header('location: password_forgot.php');
+                // Content
+                $mail->isHTML(true);
+                $mail->Subject = "{$settings->siteTitle} Password Reset";
+                $mail->Body = $message;
 
+                $mail->send();
+
+                $_SESSION['success'] = 'Password reset link sent. Please check your email.';
+            } catch (Exception $e) {
+                $_SESSION['error'] = 'Message could not be sent now. Contact support for password reset';
+            }
+        } catch (PDOException $e) {
+            $_SESSION['error'] = 'Database error occurred. Please try again later.';
+        } finally {
+            $pdo->close();
+        }
+} else {
+    $_SESSION['error'] = 'Input email associated with account';
+}
+
+header('location: password_forgot.php');
+exit;
 ?>
